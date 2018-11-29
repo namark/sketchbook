41b8f09a6b7a119b2db26a168c2ace6707f17742
namark <namark@disroot.org>
2018-11-29 12:07:50 +0400

    Quick and dirty wrappers to simplify the sketches.

---
+++ common/simple_vg.h
@@ -0,0 +1,119 @@
+#ifndef SIMPLE_VG_H
+#define SIMPLE_VG_H
+
+#include <memory>
+#include "nanovg_full.h"
+#include "simple/support/enum_flags_operators.hpp"
+#include "simple/geom/vector.hpp"
+#include "simple/graphical/color_vector.hpp"
+#include "simple/graphical/common_def.h"
+
+namespace simple::vg
+{
+	using float2 = geom::vector<float, 2>;
+	using graphical::rgb_vector;
+	using graphical::rgba_vector;
+	using graphical::rgb_pixel;
+	using graphical::rgba_pixel;
+	using range2f = support::range<float2>;
+	using rect2f = geom::segment<float2>;
+	using anchored_rect2f = geom::anchored_segment<float2>;
+
+	class frame;
+
+	class canvas
+	{
+		public:
+
+		enum class flags
+		{
+			nothing = 0,
+			antialias = NVG_ANTIALIAS,
+			stencil_strokes = NVG_STENCIL_STROKES,
+			debug = NVG_DEBUG
+		};
+
+		canvas(flags) noexcept;
+
+		canvas& clear(const rgba_vector& color = rgba_vector::white()) noexcept;
+		canvas& clear(const rgba_pixel& color) noexcept;
+
+		frame begin_frame(float2 size, float pixelRatio = 1) noexcept;
+
+		private:
+
+		struct deleter
+		{
+			void operator()(NVGcontext* context) const noexcept;
+		};
+
+		std::unique_ptr<NVGcontext, deleter> raw;
+	};
+	using ::operator |;
+	using ::operator &;
+	using ::operator &&;
+
+	class sketch;
+
+	class frame
+	{
+		public:
+			sketch begin_sketch() noexcept;
+			~frame() noexcept;
+			frame(const frame&) = delete;
+			frame(frame&&) noexcept;
+			const float2 size;
+			const float pixelRatio;
+		private:
+			NVGcontext* context;
+			frame(NVGcontext*, float2 size, float pixelRatio = 1) noexcept;
+			friend class canvas;
+	};
+
+	class sketch
+	{
+		public:
+			enum class cap
+			{
+				butt = NVG_BUTT,
+				round = NVG_ROUND,
+				square = NVG_SQUARE
+			};
+
+			enum class join
+			{
+				miter = NVG_MITER,
+				round = NVG_ROUND,
+				bevel = NVG_BEVEL
+			};
+
+			sketch& ellipse(const range2f&) noexcept;
+			sketch& rectangle(const range2f&) noexcept;
+			sketch& line(float2 from, float2 to) noexcept;
+
+			sketch& fill(const rgba_vector& color) noexcept;
+			sketch& fill(const rgba_pixel& color) noexcept;
+			sketch& fill() noexcept;
+			sketch& line_cap(cap) noexcept;
+			sketch& line_join(join) noexcept;
+			sketch& line_width(float) noexcept;
+			sketch& miter_limit(float) noexcept;
+			sketch& outline(const rgba_vector& color) noexcept;
+			sketch& outline(const rgba_pixel& color) noexcept;
+			sketch& outline() noexcept;
+
+			sketch(const sketch&) = delete;
+			sketch(sketch&&) noexcept;
+			~sketch() noexcept;
+		private:
+			NVGcontext* context;
+			sketch(NVGcontext*) noexcept;
+			friend class frame;
+	};
+
+} // namespace simple::vg
+
+template<> struct simple::support::define_enum_flags_operators<simple::vg::canvas::flags>
+	: std::true_type {};
+
+#endif /* end of include guard */
---
+++ common/sketchbook.hpp
@@ -0,0 +1,169 @@
+#include <cstring>
+#include <cstdio>
+#include <thread>
+#include <functional>
+#include <iostream>
+#include <random>
+#include <vector>
+#include <array>
+
+#include "simple/support.hpp"
+#include "simple/graphical.hpp"
+#include "simple/interactive/initializer.h"
+#include "simple/interactive/event.h"
+
+#include "simple_vg.h"
+#include "simple_vg.cpp"
+
+using namespace std::chrono_literals;
+using namespace simple;
+using namespace graphical::color_literals;
+using namespace support::literals;
+
+using support::overloaded;
+
+using graphical::gl_window;
+using graphical::int2;
+using graphical::float2;
+using rect = vg::anchored_rect2f;
+using rgb = graphical::rgb_vector;
+using rgba = graphical::rgba_vector;
+using rgb24 = graphical::rgb_pixel;
+using rgba32 = graphical::rgba_pixel;
+
+using interactive::scancode;
+using interactive::keycode;
+using interactive::mouse_button;
+
+using std::vector;
+
+constexpr int max_int = std::numeric_limits<int>::max();
+
+support::random::engine::tiny<unsigned> tiny_rand{std::random_device{}};
+support::random::distribution::naive_int<int> tiny_int_dist{0, max_int};
+support::random::distribution::naive_real<float> tiny_float_dist{0, 1};
+
+auto trand_int(decltype(tiny_int_dist)::param_type range = {0, max_int})
+{ return tiny_int_dist(tiny_rand, range); };
+
+auto trand_float(decltype(tiny_float_dist)::param_type range = {0, 1})
+{ return tiny_float_dist(tiny_rand, range); };
+
+auto trand_float2()
+{ return float2(trand_float(), trand_float()); };
+
+template <typename Number, typename Ratio = float>
+Number lerp(Number from, Number to, Ratio ratio)
+{
+	return from + (to - from) * ratio;
+}
+
+class Program
+{
+	template <typename T>
+	using o = std::optional<T>;
+	static constexpr auto no = std::nullopt;
+	using duration = std::chrono::high_resolution_clock::duration;
+
+	using draw_fun = std::function<void(frame)>;
+	using draw_loop_fun = std::function<void(frame, duration delta_time)>;
+	using key_fun = std::function<void(scancode, keycode)>;
+	using mouse_fun = std::function<void(int2)>;
+	using mouse_button_fun = std::function<void(int2, mouse_button)>;
+
+
+	bool run = true;
+	Program(const int argc, const char * const * const argv) : argc(argc), argv(argv) {}
+	friend int main(int, const char **);
+	public:
+	const int argc;
+	const char * const * const argv;
+
+	o<duration> frametime = no;
+	std::string name = "";
+	int2 size = int2(400,400);
+
+
+	// nop works ok with function pointers without having to specify template params :/
+	draw_fun draw_once = support::nop<void, frame>;
+	draw_loop_fun draw_loop = support::nop<void, frame, duration>;
+	key_fun key_down = support::nop<void, scancode, keycode>;
+	key_fun key_up = support::nop<void, scancode, keycode>;
+	mouse_fun mouse_move = support::nop<void, int2>;
+	mouse_button_fun mouse_down = support::nop<void, int2, mouse_button>;
+	mouse_button_fun mouse_up = support::nop<void, int2, mouse_button>;
+
+	bool running() { return run; }
+	void end() { run = false; }
+};
+
+void start(Program&);
+
+int main(int argc, char const* argv[]) try
+{
+	Program program{argc, argv};
+	start(program);
+
+	graphical::initializer graphics;
+	interactive::initializer interactions;
+
+	gl_window::global.require<gl_window::attribute::major_version>(2);
+	gl_window::global.request<gl_window::attribute::stencil>(8);
+	gl_window win(program.name, program.size, gl_window::flags::borderless);
+	win.require_vsync(program.frametime
+		? gl_window::vsync_mode::disabled
+		: gl_window::vsync_mode::enablded
+	);
+	float2 win_size = float2(win.size());
+
+	glewInit();
+	auto canvas = vg::canvas(vg::canvas::flags::antialias | vg::canvas::flags::stencil_strokes);
+	canvas.clear();
+
+	program.draw_once(canvas.begin_frame(win_size));
+
+	auto& now = std::chrono::high_resolution_clock::now;
+	auto t = now();
+	while(program.running())
+	{
+		using namespace interactive;
+		while(auto event = next_event()) std::visit(overloaded
+		{
+			[&program](const key_pressed& e)
+			{
+				if(!e.data.repeat)
+					program.key_down(e.data.scancode, e.data.keycode);
+			},
+			[&program](const key_released& e)
+			{
+				program.key_up(e.data.scancode, e.data.keycode);
+			},
+			[&program](const mouse_down& e)
+			{
+				program.mouse_down(e.data.position, e.data.button);
+			},
+			[&program](const mouse_up& e)
+			{
+				program.mouse_up(e.data.position, e.data.button);
+			},
+			[&program](const mouse_motion& e)
+			{
+				program.mouse_move(e.data.position);
+			},
+			[](auto) { }
+		}, *event);
+		program.draw_loop(canvas.begin_frame(win_size), now()-t);
+		t = now();
+		win.update();
+		if(program.frametime)
+			std::this_thread::sleep_for(*program.frametime);
+	}
+
+	return 0;
+}
+catch(...)
+{
+	if(errno)
+		std::puts(std::strerror(errno));
+	throw;
+}
